87127a09e65607d6099e74e11f866b6fb57e6b15
ElliotHYLee <hylee101001@gmail.com>
2018-02-05 23:23:40 -0500

    dfdfdfd

--- scripts/subdata.py
+++ scripts/subdata.py
@@ -2,7 +2,7 @@
 
 import rospy
 from std_msgs.msg import String
-from sensor_msgs.msg import Imu, Image
+from sensor_msgs.msg import Imu, Image, NavSatFix
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
@@ -14,10 +14,13 @@ class SubData:
         self.fileLock = 1
         self.dummyCounter = 0
         self.counter = 0
+        self.gps = None
         self.listener()
+
   
     def cbImu(self, data):
         q = data.orientation
+
         if self.fileLock == 0:
             if self.dummyCounter<10:
                 self.dummyCounter+=1
@@ -31,6 +34,8 @@ class SubData:
             if self.dummyCounter <10:
                self.dummyCounter+=1
             else:
+                now = rospy.get_rostime()
+                rospy.loginfo("Current time %i %i", now.secs, now.nsecs)
                 cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
                 #(rows,cols,channels) = cv_image.shape
                 #cv2.imshow("Image window", cv_image)
@@ -38,11 +43,29 @@ class SubData:
                 cv2.imwrite("Data/Image/"+str(self.counter) + ".jpg", cv_image)
                 self.fileLock = 0
 
+    def callBack(self,Nav):
+        if self.fileLock ==0:
+            self.fileLock = 1
+            self.file.write("%.4f %d %.6f %.6f\n" % (rospy.get_time(), self.counter, Nav.latitude, Nav.longitude))
+            #rospy.loginfo('I heard %s', Nav.latitude)
+            #rospy.loginfo('I heard %s', Nav.longitude)
+            self.fileLock = 0
+        else:
+            while self.fileLock==1:
+                aaaa = 1
+            self.fileLock = 1
+            self.file.write("%.4f %d %f %f\n" % (rospy.get_time(), self.counter, Nav.latitude, Nav.longitude))
+            #rospy.loginfo('I heard %s', Nav.latitude)
+            #rospy.loginfo('I heard %s', Nav.longitude)
+            self.fileLock = 0
+        
+
     def listener(self):
         rospy.init_node('mavlisten', anonymous=True)
         
         rospy.Subscriber("rgb/image_raw_color",Image,self.cbImg)
         rospy.Subscriber('mavros/imu/data', Imu, self.cbImu)
+        rospy.Subscriber('/mavros/global_position/raw/fix', NavSatFix, self.callBack)
         rospy.spin()
 
 
